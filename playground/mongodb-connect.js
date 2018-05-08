6321a7bda3ea4387b1315cf2edd6ccf1d56f2fdb
Sean Imam <seanimamo@gmail.com>
2018-05-08 14:26:39 -0400

    added ES6 destructing and example of using ObjectId() from mongodb to create a unique ID

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -2,14 +2,21 @@
 // note that this is commented out becaause we use destructing to include it instead
 //const MongoClient = require('mongodb').MongoClient;
 
-//This is how we include mongoDB within a nodejs appliaction using destructuring instead.
-const {MongoClient} = require('mongodb');
+//This is how we include mongoDB within a nodejs appliaction using destructuring instead. Notice the normal way of doing this commented above directly above.
+//Notice that we are also pulling off multiple things. this line also includes the equivalent of writing const ObjectID = require('mongodb').ObjectID;
+//The ObjectID is a function incuded in mongoDB to create unique identifiers for things(whether they are related to mongoDB or not.)
+const {MongoClient, ObjectID} = require('mongodb');
+
+// Here is an example of creating a unique object id using the objectID() function.
+// this will print out a unique mongo style string id that can serve as an ID for something we want.
+var obj = new ObjectID();
+console.log(`Example of using the ObjectID() function to create a unique mongo style string id: ${obj}`);
 
 // This is an example of an es6 feature called object destructing. It lets you create a variable that pulls an attribute from an object
 // in this example, the name attribute of the user object is pulled(aka destructured) and created into another variable called name.
 var user = {name:'sean', age:'22'};
-var {name} = user; 
-console.log(name) //this prints 'sean'
+var {name, age} = user; 
+console.log(`example of using ES6 object destructuring to create the name and age variables by pulling attributes from the user object: name: ${name}, age: ${age}`) //this prints 'sean'
 
 // This is what is used to connect to a partiuclar mongoDB. In a real production environment this url would point to something like an AWS link or a heroku app
 // It takes two arguements, the database URL followed by the name of the database you want to connect to. (Note that in this case, TodoApp did not exist, but
