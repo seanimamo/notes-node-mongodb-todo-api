ff4b00baba5f7665a16e7619fa026e6c1df44fee
Sean Imam <seanimamo@gmail.com>
2018-05-11 15:56:15 -0400

    added server.js file for mongoose

---
+++ server/server.js
@@ -0,0 +1,8 @@
+//this server.js file will use mongoose rather than mongodb
+var mongoose  = require('mongoose');
+
+mongoose.connect('mongodb://localhost:271017/TodoApp',(err,db)=>{
+
+})
+
+//save new something
